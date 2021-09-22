6c5cdedc91d27010d849edc4f9bd5be6ba5947a5
Mayur <mayurdange@yahoo.co.in>
2021-09-22 10:26:38 +0530

    reading json to calculate count

--- src/main/java/MessageListener.java
+++ src/main/java/MessageListener.java
@@ -1,23 +1,40 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.ChannelType;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
+import net.dv8tion.jda.internal.utils.tuple.Pair;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import javax.security.auth.login.LoginException;
 import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static net.dv8tion.jda.api.interactions.commands.OptionType.STRING;
 
 public class MessageListener extends ListenerAdapter {
+    private static Map<String, Clans> allClans = new HashMap<>();
+    private static RestTemplate restTemplate = new RestTemplate();
+    private Map<Pair<Integer, Integer>, Integer> weightRange = new HashMap<>();
+
+    public MessageListener() {
+        weightRange.put(Pair.of(139000, 116000), 14);
+        weightRange.put(Pair.of(116000, 119000), 13);
+        weightRange.put(Pair.of(119000, 109000), 12);
+        weightRange.put(Pair.of(109000, 86000), 11);
+        weightRange.put(Pair.of(86000, 69000), 10);
+        weightRange.put(Pair.of(69000, 0), 9);
+    }
+
     public static void main(String[] args)
             throws LoginException {
         JDA jda = JDABuilder.createLight(args[0], EnumSet.noneOf(GatewayIntent.class)) // slash commands don't need any intents
@@ -26,7 +43,7 @@ public class MessageListener extends ListenerAdapter {
         // These commands take up to an hour to be activated after creation/update/delete
         CommandListUpdateAction commands = jda.updateCommands();
         commands.addCommands(
-                new CommandData("compo", "gets the TH composition of the clan")
+                new CommandData("comp", "gets the TH composition of the clan")
                         .addOptions(new OptionData(STRING, "content", "Clan tag. if blank, use default clans")
                                 .setRequired(false))
         );
@@ -40,7 +57,7 @@ public class MessageListener extends ListenerAdapter {
         if (event.getGuild() == null)
             return;
         switch (event.getName()) {
-            case "compo":
+            case "comp":
                 calcStats(event); // content is required so no null-check here
                 break;
             default:
@@ -50,12 +67,47 @@ public class MessageListener extends ListenerAdapter {
 
     private void calcStats(SlashCommandEvent event) {
         OptionMapping clanTag = event.getOption("content");
-        //get https://fwastats.com/Clans.json
-        //match hashtag
-        //get https://fwastats.com/Clan/9JUVCV0L/Members.json
-        //get https://fwastats.com/Weights.json
-        //for each member get the weight
-        //as per weight range for TH calculate the message
-        event.reply(":townhall14:").queue(); // This requires no permissions!
+        event.reply("calculating..").queue();
+        if (allClans.isEmpty()) {
+            ResponseEntity<List<Clans>> responseClans =
+                    restTemplate.exchange("https://fwastats.com/Clans.json",
+                            HttpMethod.GET, null, new ParameterizedTypeReference<List<Clans>>() {
+                            });
+            responseClans.getBody().forEach(c -> {
+                allClans.put(c.getTag(), c);
+            });
+        }
+        Clans clan = allClans.get(clanTag == null ? "#9JUVCV0L" : clanTag);
+
+        ResponseEntity<List<Members>> responseMembers =
+                restTemplate.exchange("https://fwastats.com/Clan/" + clan.getTag().substring(1) + "/Members.json",
+                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Members>>() {
+                        });
+
+        Map<Integer, Integer> actual = new HashMap<>();
+        Map<Integer, Integer> weight = new HashMap<>();
+        responseMembers.getBody().forEach(m -> {
+            Integer thLvl = actual.getOrDefault(m.getTownHall(), 0);
+            actual.put(m.getTownHall(), ++thLvl);
+
+            weightRange.forEach((p, t) -> {
+                if (m.getWeight() > p.getLeft() && m.getWeight() < p.getRight()) {
+                    Integer w = weight.getOrDefault(t, 0);
+                    weight.put(t, ++w);
+                }
+            });
+        });
+
+        String act = "Actual: ";
+        String wt = "Weight: ";
+        String th = "TH: ";
+        for (int i = 14; i > 7; i--) {
+            Integer act1 = actual.getOrDefault(i, 0);
+            act = act + "/" + act1;
+            Integer wt1 = weight.getOrDefault(i, 0);
+            wt = wt + "/" + wt1;
+            th = th + "/" + i;
+        }
+        event.getMessageChannel().sendMessage(th + "\n" + act + "\n" + wt).queue(); // This requires no permissions!
     }
 }
