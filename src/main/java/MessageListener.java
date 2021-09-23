2b37a428c5722bc87b4495e9bfc8d4e6acc177ad
mayur <mayurdange@yahoo.co.in>
2021-09-23 11:05:18 +0530

    changed horizontal to vertical layout

--- src/main/java/MessageListener.java
+++ src/main/java/MessageListener.java
@@ -1,34 +1,38 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
+import net.dv8tion.jda.api.requests.restaction.WebhookMessageAction;
 import net.dv8tion.jda.internal.utils.tuple.Pair;
-import org.apache.commons.logging.Log;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import javax.imageio.ImageIO;
 import javax.security.auth.login.LoginException;
-import java.io.*;
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static net.dv8tion.jda.api.interactions.commands.OptionType.STRING;
 
 public class MessageListener extends ListenerAdapter {
-    private static Map<String, Clans> allClans = new HashMap<>();
-    private static RestTemplate restTemplate = new RestTemplate();
-    private Map<Pair<Integer, Integer>, Integer> weightRange = new HashMap<>();
+    private static final Map<String, Clans> allClans = new HashMap<>();
+    private static final RestTemplate restTemplate = new RestTemplate();
+    private final Map<Pair<Integer, Integer>, Integer> weightRange = new HashMap<>();
+    private final Map<Integer, String> emojiMap = new HashMap<>();
 
     public MessageListener() {
         weightRange.put(Pair.of(Integer.MAX_VALUE / 1000, 131), 14);
@@ -37,42 +41,39 @@ public class MessageListener extends ListenerAdapter {
         weightRange.put(Pair.of(111, 91), 11);
         weightRange.put(Pair.of(91, 71), 10);
         weightRange.put(Pair.of(71, 56), 9);
-        weightRange.put(Pair.of(56, 42), 8);
+        weightRange.put(Pair.of(56, 0), 8);
+
         ObjectMapper mapper = new ObjectMapper();
         try {
             Clans[] clansOp = mapper.readValue(new File("src/main/resources/Clans.json"), Clans[].class);
-            for (Clans clan: clansOp) {
-                allClans.put(clan.getTag(),clan);
+            for (Clans clan : clansOp) {
+                allClans.put(clan.getTag(), clan);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        emojiMap.put(14, "<:th_14:881852620485050369>");
+        emojiMap.put(13, "<:th_13:881852471255900181>");
+        emojiMap.put(12, "<:th_12:881852800248741948>");
+        emojiMap.put(11, "<:th_11:881854521003900999>");
+        emojiMap.put(10, "<:th_10:881854603023503360>");
+        emojiMap.put(9, "<:th_9:881854733151793152>");
     }
 
     public static void main(String[] args)
             throws LoginException {
-        JDA jda = JDABuilder.createLight(args[0], EnumSet.noneOf(GatewayIntent.class)) // slash commands don't need any intents
-                .addEventListeners(new MessageListener())
-                .build();
-        // These commands take up to an hour to be activated after creation/update/delete
+        JDA jda = JDABuilder.createLight(args[0], EnumSet.noneOf(GatewayIntent.class))
+                .addEventListeners(new MessageListener()).build();
         CommandListUpdateAction commands = jda.updateCommands();
-        commands.addCommands(
-                new CommandData("comp", "gets the TH composition of the clan")
-                        .addOptions(new OptionData(STRING, "tag", "Clan tag. if blank, use default clans")
-                                .setRequired(false))
+        commands.addCommands(new CommandData("comp", "gets the TH composition of the clan")
+                .addOptions(new OptionData(STRING, "tag", "Clan tag. if blank, use default clans").setRequired(false))
         );
-
-        commands.addCommands(
-                new CommandData("abc", "description")
-        );
-        // Send the new set of commands to discord, this will override any existing global commands with the new set provided here
         commands.queue();
     }
 
     @Override
     public void onSlashCommand(SlashCommandEvent event) {
-        // Only accept commands from guilds
-        if (event.getGuild() == null)
+        if (event.getGuild() == null)// Only accept commands from guilds
             return;
         switch (event.getName()) {
             case "comp":
@@ -84,68 +85,61 @@ public class MessageListener extends ListenerAdapter {
     }
 
     public void calcStats(SlashCommandEvent event) {
-        OptionMapping clanTag = event.getOption("tag");
-        //event.reply("calculating..").queue();
+        String clanTagstr = event.getOption("tag") == null ? "#9JUVCV0L" : event.getOption("tag").getAsString();
         event.deferReply().queue();
-        String responseMessage = calculateThComp(clanTag);
-        //event.getMessageChannel().sendMessage(responseMessage).queue(); // This requires no permissions!
-        //event.reply(responseMessage).queue();
-        event.getHook().sendMessage(responseMessage).queue();
+        String responseMessage = calculateThComp(clanTagstr);
+        WebhookMessageAction<Message> messageWebhookMessageAction = event.getHook().sendMessage(responseMessage);
+        try {
+            URL url = new URL(allClans.get(clanTagstr).getImage());
+            BufferedImage img = ImageIO.read(url);
+            File file = new File("temp.png"); // change the '.jpg' to whatever extension the image has
+            ImageIO.write(img, "png", file); // again, change 'jpg' to the correct extension
+            messageWebhookMessageAction = messageWebhookMessageAction.addFile(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        messageWebhookMessageAction.queue();
     }
 
-    @NotNull
-    private String calculateThComp(OptionMapping clanTag) {
-        String clanTagstr =  clanTag== null ? "#9JUVCV0L" : clanTag.getAsString();
-        System.out.println(clanTagstr);
+    private String calculateThComp(String clanTagstr) {
         ResponseEntity<List<Members>> responseMembers = getMemberInfo(clanTagstr);
-        String responseMessage = getResponseString(responseMembers.getBody(),clanTagstr);
-        return responseMessage;
+        return getResponseString(responseMembers.getBody(), clanTagstr);
     }
 
-    @NotNull String getResponseString(List<Members> members,String clanTagstr) {
+    @NotNull String getResponseString(List<Members> members, String clanTagstr) {
         Map<Integer, Integer> actual = new HashMap<>();
         Map<Integer, Integer> weight = new HashMap<>();
+        List<Members> missingWeights = new ArrayList<>();
         for (Members m : members) {
             Integer thLvl = actual.getOrDefault(m.getTownHall(), 0);
             actual.put(m.getTownHall(), ++thLvl);
-
-            for (Map.Entry<Pair<Integer, Integer>, Integer> entry : weightRange.entrySet()) {
-                Pair<Integer, Integer> p = entry.getKey();
-                Integer t = entry.getValue();
-                Integer memberWeight = m.getWeight() / 1000;
-                if (memberWeight < p.getLeft() && memberWeight > p.getRight()) {
-                    Integer w = weight.getOrDefault(t, 0);
-                    weight.put(t, ++w);
-                    break;
+            if (m.getWeight() == 0) {
+                missingWeights.add(m);
+            } else {
+                for (Map.Entry<Pair<Integer, Integer>, Integer> entry : weightRange.entrySet()) {
+                    Pair<Integer, Integer> p = entry.getKey();
+                    Integer t = entry.getValue();
+                    int memberWeight = m.getWeight() / 1000;
+                    if (memberWeight <= p.getLeft() && memberWeight > p.getRight()) {
+                        weight.compute(t, (k, v) -> v == null ? 1 : v + 1);
+                        break;
+                    }
                 }
             }
         }
-
-        System.out.println("actual" + actual + "\nweight" + weight);
-
-        String act = "\nActual:\t";
-        String wt = "\nWeight:  ";
-        String th = "\nTH:\t\t\t<:th_14:881852620485050369>\t<:th_13:881852471255900181>\t<:th_12:881852800248741948>\t<:th_11:881854521003900999>\t<:th_10:881854603023503360>\t<:th_9:881854733151793152>";
-        for (int i = 14; i > 7; i--) {
-            Integer act1 = actual.get(i);
-            act = act + "\t" + formattedWeight(act1) + "  ";
-            Integer wt1 = weight.get(i);
-            wt = wt + "\t" + formattedWeight(wt1 ) + "  ";
+        StringBuilder s = new StringBuilder("TH count for clan **").append(allClans.get(clanTagstr).getName()).append("**\nTH\tlvl\tweight\n");
+        for (int i = 14; i > 8; i--) {
+            s.append(String.format("%5s %5d %5d\n", emojiMap.get(i), actual.getOrDefault(i, 0), weight.getOrDefault(i, 0)));
         }
-        String responseMessage = "TH count for clan *"+allClans.get(clanTagstr).getName()+"*"+th + act + wt;
-        return responseMessage;
+        if (!missingWeights.isEmpty()) {
+            s.append("\nweight missing for ")
+                    .append(missingWeights.stream().map(Members::getName).collect(Collectors.joining(", ")))
+                    .append("\n Please update the war weight at https://fwastats.com/Clan/").append(clanTagstr.substring(1))
+                    .append("/Weight");
+        }
+        return s.toString();
     }
 
-    private String formattedWeight(Integer i) {
-        if (i==null){
-            return "  ";
-        }else if (i<10){
-            return " "+i;
-        }
-        return  String.valueOf(i);
-    }
-
-    @NotNull
     private ResponseEntity<List<Members>> getMemberInfo(String clanTagstr) {
         ResponseEntity<List<Members>> responseMembers =
                 restTemplate.exchange("https://fwastats.com/Clan/" + clanTagstr.substring(1) + "/Members.json",
@@ -154,13 +148,9 @@ public class MessageListener extends ListenerAdapter {
         return responseMembers;
     }
 
-    private void populateClanInfo() {
-        ResponseEntity<List<Clans>> responseClans =
-                restTemplate.exchange("https://fwastats.com/Clans.json",
-                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Clans>>() {
-                        });
-        responseClans.getBody().forEach(c -> {
-            allClans.put(c.getTag(), c);
-        });
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        super.onMessageReceived(event);
+        System.out.println(event.getMessage().getContentRaw());
     }
 }
--- src/test/java/MessageListenerTest.java
+++ src/test/java/MessageListenerTest.java
@@ -1,24 +1,13 @@
-import net.dv8tion.jda.internal.utils.tuple.Pair;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class MessageListenerTest {
 
     @Test
     void calcStats() {
         ArrayList<Members> members = new ArrayList<>();
-
-        //        weightRange.put(Pair.of(139000, 116000), 14);
-//        weightRange.put(Pair.of(116000, 119000), 13);
-//        weightRange.put(Pair.of(119000, 109000), 12);
-//        weightRange.put(Pair.of(109000, 86000), 11);
-//        weightRange.put(Pair.of(86000, 69000), 10);
-//        weightRange.put(Pair.of(69000, 0), 9);
-
         members.add(getMembers(138000, 14));
         members.add(getMembers(118000, 13));
         members.add(getMembers(108000, 12));
